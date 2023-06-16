aaa9a5645594ec46d3e98b9f0c5b0fbf514bf3f1
caren <carenoliveirasilvapontes@gmail.com>
2023-06-16 15:25:07 -0300

    feat:atualização de pacote,browserRouter, e rotas no app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+
+      </Switch>
+    );
+  }
+}
+
+export default App;
