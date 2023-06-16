d33ba54807aa123cc3636f44905ed7ed31baa5f8
caren <carenoliveirasilvapontes@gmail.com>
2023-06-16 15:34:36 -0300

    feat:category controller iniciando

---
+++ projetoForum/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request)
+    {
+        $category=Category::where('name',$request->input('name'))->first();
+        if(!$category){
+            return response([
+                'message'=>'Category not found'
+            ],404);
+        }
+        $result = Post::where('category_id',$category->id)->get();
+        if(!$result){
+            return response([
+                'message'=>'not exist none post about that category'
+            ],404);
+        }
+        return response($result,200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
