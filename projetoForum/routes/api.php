383b34e823ed14fdb51835e99a48f1528a77647c
caren <carenoliveirasilvapontes@gmail.com>
2023-06-16 15:36:07 -0300

    feat:seeder user e rotas

--- projetoForum/routes/api.php
+++ projetoForum/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::get('/category',[CategoryController::class,'show']);
+Route::get('/post',[PostController::class,'index']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/post',[PostController::class,'store']);
+
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('user',[AuthController::class,'user']);
+    
 });
