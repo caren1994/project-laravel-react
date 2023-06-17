17b68cc178d3c4d3039e2c2ba33250528cffeb13
caren <carenoliveirasilvapontes@gmail.com>
2023-06-17 14:50:24 -0300

    feat:adicionando rota register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import React from "react";
+import "./index.css";
+import { Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={ Login } />
+	render () {
+		return (
+			<Switch>
+				<Route exact path="/" component={ Login } />
+				<Route exact path="/register" component={ Register } />
 
-      </Switch>
-    );
-  }
+			</Switch>
+		);
+	}
 }
 
 export default App;
