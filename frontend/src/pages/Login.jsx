cd29963dea69a24f7047e73dd4da4c29b2be8c50
caren <carenoliveirasilvapontes@gmail.com>
2023-06-17 14:51:53 -0300

    feat: adicionando validação de email e senha , adicionando estados

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,84 +1,78 @@
 import { useEffect, useState } from "react";
-import {AiOutlineUser} from 'react-icons/ai';
+import { AiOutlineUser } from "react-icons/ai";
 import axios from "axios";
-export default function Login(){
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+export default function Login () {
+	const [ email, setEmail ] = useState("");
+	const [ password, setPassword ] = useState("");
+	const [ isDisabled, setDisabled ] = useState(true);
+	const [ error, setError ] = useState(null);
+	const [ user, setUser ] = useState("");
+	const MIN_LENGTH_PASSWORD = 5;
 
+	useEffect(() => {
+		axios.defaults.withCredentials = true;
+		axios.get("http://localhost:8000/sanctum/csrf-cookie").then((data) => {
+			console.log(data);
+		});
+	}, []);
+	useEffect(() => {
+		const vEmail = /^\S+@\S+\.\S+$/;
+		const isEmailvalid = email.match(vEmail) != null;
+		const isPasswordvalid = password.length >= MIN_LENGTH_PASSWORD;
+		setDisabled(!(isEmailvalid && isPasswordvalid));
+	}, [ password, email ]);
 
-// const [firstName, setFirstName] = useState("");
-// const [lastName, setLastName] = useState("");
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
+	const handleSubmit = () => {
+		try {
+			const token = document.cookie.split("=")[1];
+			console.log(token);
+			const { data } = axios.post("http://localhost:8000/api/login", {
+				email,
+				password
+			}, { headers: { "Content-Type": "application/json", "X-XSRF-TOKEN": token } });
+		} catch (err) {
+			setError(err.response.data.message);
+		}
+	};
+	return (
 
-useEffect(()=>{
-axios.defaults.withCredentials = true;
-axios.get("http://localhost:8000/sanctum/csrf-cookie").then((data)=>{
-  console.log(data)});
-  
-},[]); 
-// axios({
-//   method: 'post',
-//   url: '/user/12345',
-//   data: {
-//     nome: 'Victor',
-//     sobrenome: 'Nogueira'
-//   }
-// });
+	// flex min-h-screen flex-col items-center justify-between p-24
+		<>
+			<main className=" mainPage">
+				{/* <div className="bg-white max-w-6xl border-4 border-solid border-red-900   "> */}
+				<form className="formulario  ">
+					<div className=" divCircle bg-fuchsia-800 ">
+						<span style={{ fontSize: "3.5em", color: "white" }}>
+							<AiOutlineUser
+							/>
+						</span>
+					</div>
+					<div className=" divInput">
+						<label htmlFor="email" className=" label">
+							<input type="email" name="email" id="email" placeholder="email"
+								className="input"
+								value={email}
+								onChange={({ target }) => setEmail(target.value)} />
+						</label>
 
-const handleSubmit = () => {
-  const token=document.cookie.split("=")[1];
-  console.log(token);
+						<label htmlFor="password" className=" label" >
+							<input type="password" name="password" id="password" placeholder="password"
+								className="input"
+								value={password}
+								onChange={({ target }) => setPassword(target.value)} />
+						</label>
+						<Link to="/register" className="mb-9 text-orange-400 ">Não possui cadastro?</Link>
+						<button type="button" className=" button bg-orange-300 " disabled={ isDisabled } onClick={handleSubmit}>Enter</button>
+						{error && (
+							<p className="text-red-700">
+								{error}
+							</p>)}
 
-axios.post("http://localhost:8000/api/login",{
-  email,
-  password
-},{headers: {"Content-Type":"application/json","X-XSRF-TOKEN":token}})
-.then((data)=>console.log(data)).catch((err)=>console.log(err));
-
-}
-return (
-  <>
-  <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-200  ">
-    <div className="bg-white w-4/12 h-5/6">
-    <form className="flex flex-col bg-white shadow-xl w-400 max-h-80 ">
-      <div className=" w-20 h-20 bg-fuchsia-800 m-auto rounded-full
-       flex justify-center items-center mt-3 mb-3 ">
-        <span style={{fontSize:"2.0em" ,color:"white"}}>
-          <AiOutlineUser
-          />
-        </span>
-      </div>
-
-      {/* <label htmlFor="firstName" >
-      <input type="text" name="fistName" id="firtsName" placeholder="firstName"
-        className="border-solid border-2 bg-slate-200 "
-        value={firstName}
-        onChange={({target})=>setFirstName(target.value)} />
-      </label>
-
-      <label htmlFor="lastName" >
-      <input type="text" name="lastName" id="lastName" placeholder="lastName"
-        className="border-style: solid "
-        value={lastName}
-        onChange={({target})=>setLastName(target.value)} />
-      </label> */}
-
-      <label htmlFor="email" >
-      <input type="email" name="email" id="email" placeholder="email"
-        className="border-style: solid "
-        value={email}
-        onChange={({target})=>setEmail(target.value)} />
-      </label>
-
-      <label htmlFor="password" >
-      <input type="password" name="password" id="password" placeholder="password"
-        className="border-style: solid "
-        value={password}
-        onChange={({target})=>setPassword(target.value)} />
-      </label>
-      <button type="button" className=" bg-fuchsia-800 rounded text-white " onClick={handleSubmit}>Enter</button>
-    </form>
-    </div>
-  </main>
-  </>
-)
+					</div>
+				</form>
+				{/* </div> */}
+			</main>
+		</>
+	);
 }
